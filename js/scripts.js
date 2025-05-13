e9ffb030f0f80e6e6595dea2abfaf9695b3a21d4
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-05-13 13:47:28 +0100

    Delete js directory

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").classList.remove("hidden");
-    }, 2000); // Adjust load time as needed
-})
