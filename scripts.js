2e7d6aaba4c2ccd3d53a5c1de366b0462643d596
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-05-13 14:03:20 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Show loading animation, then reveal content after 2 seconds
-    setTimeout(() => {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").classList.remove("hidden");
-    }, 2000); // Adjust timing as needed
-})
+    document.getElementById("content").classList.remove("hidden");
+});
